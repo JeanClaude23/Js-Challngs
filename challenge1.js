a039df8c429e2ee219127b91556f7c8ac3921f17
Code Sandbox <hello@codesandbox.io>
2022-04-01 15:22:21 +0000

    Deployment from Codesandbox.

---
+++ challenge1.js
@@ -0,0 +1,6 @@
+var name = ["david", "jackson", "thiery", "christian"];
+name[2] = "jerome";
+name[3] = "ajay";
+name.unshift("frida");
+console.log(name);
+document.write(name);
---
+++ challenge2.js
@@ -0,0 +1,7 @@
+let greeting = ["hllo", "class", "world!", "hello", "hllo"];
+console.log("orginal arry: ", greeting);
+greeting.pop();
+greeting.pop();
+console.log("ofter remove two last element: ", greeting);
+console.log("covernt to string : ", greeting.toString());
+console.log(greeting.join(" "));
